fc62ed52e3acad75f88f08370a88f6a9e5b6fa14
pritam ramteke <pritam.teke@exceloid.com>
2021-01-07 23:01:36 +0530

    testing

--- app/src/androidTest/java/com/example/damienaicheh/myandroidcrazyapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/damienaicheh/myandroidcrazyapp/ExampleInstrumentedTest.java
@@ -1,10 +1,9 @@
 package com.demo.damienaicheh.myandroidcrazyapp;
-package androidx.test.internal.runner.junit3;
 import android.content.Context;
 // import android.support.test.InstrumentationRegistry;
 // import android.support.test.runner.AndroidJUnit4;
 import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
