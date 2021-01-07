76b512a83f7f52ec2867cdd12a3b69eed164faf8
pritam ramteke <pritam.teke@exceloid.com>
2021-01-08 00:18:27 +0530

    final test

--- app/src/androidTest/java/com/example/damienaicheh/myandroidcrazyapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/damienaicheh/myandroidcrazyapp/ExampleInstrumentedTest.java
@@ -2,7 +2,8 @@ package com.demo.damienaicheh.myandroidcrazyapp;
 import android.content.Context;
 // import android.support.test.InstrumentationRegistry;
 // import android.support.test.runner.AndroidJUnit4;
-import androidx.test.InstrumentationRegistry;
+// import androidx.test.InstrumentationRegistry;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +20,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
+ //       Context appContext = InstrumentationRegistry.getTargetContext();
+         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.demo.damienaicheh.myandroidcrazyapp", appContext.getPackageName());
     }
 }
